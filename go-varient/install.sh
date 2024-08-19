2d410f8ed73db3c7dc14aa6ee0203c0bdfbc519e
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-08-19 14:12:36 -0400

    Delete go-varient directory

--- go-varient/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p ~/.binaries/
-cd ~/.binaries/
-wget https://github.com/soltros/nixpkg.py/raw/main/go-varient/nixpkg
-chmod +x nixpkg
-echo 'alias nixpkg="sudo ~/.binaries/./nixpkg"' >> ~/.bashrc
-source ~/.bashrc
