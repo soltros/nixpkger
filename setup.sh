c45282316bfdf7304a584961d64c724c2e502a75
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-08-30 02:13:47 -0400

    v2 reversion.
    Accidentally deployed the v3 rewrite to main. Reverting everything back to v2, as v3 isn't fully operational.

--- setup.sh
+++ setup.sh
@@ -1,32 +1,13 @@
-#!/bin/bash
+nix-env -iA nixos.python311Full
 
-## Setup env
-nix-env -iA nixos.python311Full nixos.wget
+git clone https://github.com/soltros/nixpkg.py
 
-## Download files
-cd ~/Downloads/
-wget https://raw.githubusercontent.com/soltros/nixpkger/main/install.py
+mkdir -p ~/scripts/
+
+cp ~/nixpkg.py/nixpkg.py ~/scripts/
+
+echo -e "\nalias nixpkg='sudo python ~/scripts/nixpkg.py'" >> ~/.bashrc
+
 wget https://raw.githubusercontent.com/soltros/configbuilder/main/modules/apps.nix
-python install.py
 
-## Place apps.nix
-sudo mv apps.nix /etc/nixos/
-
-## Add alias to .bashrc
-alias_line="alias nixpkg='sudo python ~/scripts/nixpkg.py'"
-
-if [ -f "$HOME/.bashrc" ]; then
-    if grep -qF "$alias_line" "$HOME/.bashrc"; then
-        echo "Alias 'nixpkg' already exists in .bashrc."
-    else
-        echo "Adding alias 'nixpkg' to .bashrc..."
-        echo "$alias_line" >> "$HOME/.bashrc"
-        echo "Alias added successfully."
-    fi
-else
-    echo "Creating a new .bashrc file..."
-    echo "$alias_line" > "$HOME/.bashrc"
-    echo "Alias 'nixpkg' added to .bashrc."
-fi
-
-echo "Setup complete."
+sudo cp apps.nix /etc/nixos/
