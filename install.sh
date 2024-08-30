88389157d07cda44a8b8308fbcc0316e8b8a7a53
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-08-29 22:57:50 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Install necessary packages
-nix-env -iA nixos.python311Full nixos.unzip
+nix-env -iA nixos.python311Full nixos.wget nixos.unzip
 
 # Download the main.zip file
-curl -f -o /tmp/main.zip https://github.com/soltros/nixpkger/archive/refs/heads/main.zip || { echo "Failed to download main.zip"; exit 1; }
+wget https://github.com/soltros/nixpkger/archive/refs/heads/main.zip -O /tmp/main.zip || { echo "Failed to download main.zip"; exit 1; }
 
 # Create the scripts directory
 mkdir -p ~/scripts/
@@ -14,22 +14,18 @@ mkdir -p ~/scripts/python/
 unzip /tmp/main.zip -d /tmp/ || { echo "Failed to unzip main.zip"; exit 1; }
 
 # Move files from the unzipped directory to the final destination
-mv "/tmp/nixpkger-main/*" ~/scripts/ || { echo "Failed to move files"; exit 1; }
-mv "~/scripts/*.py" ~/scripts/python/
+mv /tmp/nixpkger-main/* ~/scripts/ || { echo "Failed to move files"; exit 1; }
+mv ~/scripts/*.py ~/scripts/python/ || echo "Failed to move python files"; exit 1
 
 # Clean up temporary files
-rm "/tmp/main.zip"
-rm -rf "/tmp/nixpkger-main/"
+rm /tmp/main.zip
+rm -rf /tmp/nixpkger-main/
 
 # Add alias to .bashrc if it doesn't exist
 grep -qxF "alias nixpkger='bash ~/scripts/nixpkger'" ~/.bashrc || echo "alias nixpkger='bash ~/scripts/nixpkger'" >> ~/.bashrc
 
 # Download and copy the apps.nix file
-if [ ! -f /etc/nixos/apps.nix ]; then
-  curl -f -o /tmp/apps.nix https://raw.githubusercontent.com/soltros/configbuilder/main/modules/apps.nix || { echo "Failed to download apps.nix"; exit 1; }
-  sudo cp /tmp/apps.nix /etc/nixos/ || { echo "Failed to copy apps.nix to /etc/nixos/"; exit 1; }
-else
-  echo "apps.nix already exists in /etc/nixos/, skipping download and copy"
-fi
+wget https://raw.githubusercontent.com/soltros/configbuilder/main/modules/apps.nix -O /tmp/apps.nix || { echo "Failed to download apps.nix"; exit 1; }
+sudo cp /tmp/apps.nix /etc/nixos/ || { echo "Failed to copy apps.nix to /etc/nixos/"; exit 1; }
 
 echo "Setup completed successfully!"
