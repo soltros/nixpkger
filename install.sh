ada3506f9171c3594481a50a72559d94ada56156
Derrik Diener <142091676+soltros@users.noreply.github.com>
2025-03-28 04:19:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,22 +3,13 @@
 # Install necessary packages
 nix-env -iA nixos.python311Full nixos.wget nixos.unzip
 
-# Download the main.zip file
-wget https://github.com/soltros/nixpkger/archive/refs/heads/main.zip -O v3.zip
-
-# Unzip the downloaded file
-unzip v3.zip
-mv nixpkger-main/ ~/scripts/
+##Install
+git clone https://github.com/soltros/nixpkger
+cd nixpkger/
+mkdir -p ~/scripts/
 mkdir -p ~/scripts/python/
-cd ~/scripts/
-mv *.py python/
-chmod +x nixpkger
+mv *.py ~/scripts/python/
+mv resources/ ~/scripts/
+mv nixpkger ~/scripts
 
-# Add alias to .bashrc if it doesn't exist
-grep -qxF "alias nixpkger='bash ~/scripts/nixpkger'" ~/.bashrc || echo "alias nixpkger='bash ~/scripts/nixpkger'" >> ~/.bashrc
-
-# Download and copy the apps.nix file
-wget https://raw.githubusercontent.com/soltros/configbuilder/main/modules/apps.nix
-sudo cp apps.nix /etc/nixos/
-
-echo "Setup completed successfully!"
+echo "Be sure to replace main.py with the flake variant if you plan to use flakes with Nixpkger.
